c3a2727488eb9c992726387e761bb315795d45d9
Dieguidev <diegogaraycullas@gmail.com>
2024-08-16 12:33:56 -0500

    feat: Add password confirmation validation to RegisterUserDto
    This commit adds password confirmation validation to the `RegisterUserDto` class in the `register-user.dto.ts` file. It ensures that the `password` and `passwordConfirmation` fields match, providing an extra layer of security during user registration. This enhancement improves the reliability and integrity of the registration process.

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -13,14 +13,15 @@ export class RegisterUserDto {
   ) { }
 
   static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
-    const { name, email, password } = object;
+    const { name, email, password, passwordConfirmation } = object;
 
     if (!name) return ['Missing name'];
     if (!email) return ['Missing email'];
     if(!Validators.email.test(email)) return ['Invalid email'];
     if (!password) return ['Missing password'];
     if (password.length < 6) return ['Password must be at least 6 characters'];
-
+    if (!passwordConfirmation) return ['Missing password confirmation'];
+    if (password !== passwordConfirmation) return ['Passwords do not match'];
 
     return [undefined, new RegisterUserDto(name, email.toLowerCase(), password)];
 
