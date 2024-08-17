df90766bae8292b9a057b087e658cac4b9581280
Dieguidev <diegogaraycullas@gmail.com>
2024-08-17 14:20:24 -0500

    chore: Update WEBSERVICE_URL configuration option
    The commit updates the `WEBSERVICE_URL` configuration option in the `envs.ts` file. It changes the requirement for the option from `required` to `optional`, allowing it to be set as an empty string if not provided. This change provides flexibility in case the webservice URL is not needed in certain environments.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,7 +7,7 @@ export const envs = {
   MONGO_URL: get('MONGO_URL').required().asString(),
   MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
   JWT_SECRET: get('JWT_SECRET').required().asString(),
-  WEBSERVICE_URL: get('WEBSERVICE_URL').required().asString(),
+  WEBSERVICE_URL: get('WEBSERVICE_URL').asString(),
   SEND_EMAIL: get('SEND_EMAIL').required().asBool(),
   MAILER_SERVICE: get('MAILER_SERVICE').required().asString(),
   MAILER_EMAIL: get('MAILER_EMAIL').required().asString(),
